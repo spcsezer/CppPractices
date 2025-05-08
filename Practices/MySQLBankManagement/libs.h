be2ed1aa388479cb5c48a13c0a888e9cce614352
sezer <82259871+spcsezer@users.noreply.github.com>
2025-05-08 09:28:49 +0300

    Create libs.h

---
+++ Practices/MySQLBankManagement/libs.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <mysql/jdbc.h>
+#include <random>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
